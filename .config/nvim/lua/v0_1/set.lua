96998c736f1f7ab4834566e0f3afb432cd827877
sekerhalithamza_wsl <shalithamza@gmail.com>
2024-06-02 17:45:19 +0300

    get rid of undodir files

--- .config/nvim/lua/v0_1/set.lua
+++ .config/nvim/lua/v0_1/set.lua
@@ -13,8 +13,6 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.config/nvim/undodir"
-vim.opt.undofile = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
